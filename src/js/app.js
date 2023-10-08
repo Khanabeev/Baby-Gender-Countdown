e111c88ee0c6f2dab994fedc13bf82763de74b50
MyCode07 <erem19992015@mail.ru>
2023-10-08 12:59:27 +0300

    hover effects add

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,13 @@ import "./static/side-fixed.js";
 import "./parts/menu.js";
 import "./parts/slider.js";
 import "./parts/langs.js";
-import "./parts/countdown.js";
 import "./parts/animations.js";
 
 
 import { replaceDomElements } from "./static/replace.js";
 import { accorden } from "./static/accordeon.js";
+import { clock } from "./parts/countdown.js";
 
 replaceDomElements();
 accorden();
+clock();
--- src/js/parts/langs.js
+++ src/js/parts/langs.js
@@ -1,35 +1,29 @@
-
-
-
-
-let allLang = [];
 const langsElem = document.querySelector('.multilanguage');
-const openBtn = langsElem.querySelector('button');
-const label = langsElem.querySelector('button label');
-const langs = langsElem.querySelectorAll('li a');
-
-openBtn.addEventListener('click', (е) => {
-    langsElem.classList.toggle('_open');
-})
 
 
-langs.forEach(lang => {
-    const langCode = lang.textContent.toLowerCase();
-    allLang.push(langCode)
+if (langsElem) {
+    const openBtn = langsElem.querySelector('button');
+    const label = langsElem.querySelector('button label');
+    const langs = langsElem.querySelectorAll('li a');
 
-
-
-    lang.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        label.textContent = lang.textContent
-        langsElem.classList.remove('_open')
-
-        langs.forEach(item => {
-            item.classList.remove('_selected')
-        })
-
-        lang.classList.add('_selected')
+    openBtn.addEventListener('click', (е) => {
+        langsElem.classList.toggle('_open');
     })
-})
 
+
+    if (langs.length)
+        langs.forEach(lang => {
+            lang.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                label.textContent = lang.textContent
+                langsElem.classList.remove('_open')
+
+                langs.forEach(item => {
+                    item.classList.remove('_selected')
+                })
+
+                lang.classList.add('_selected')
+            })
+        })
+}
